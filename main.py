2a5f1f98b19e61f50acac8860313f9f4c5b14e1b
Вечканов Алексей <avechkanov@alfabank.ru>
2023-06-04 12:23:04 +0300

    added space ship movements

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ import os
 # Подготавливаем необходимые модули Pygame
 pygame.init()
 
+### Константы ###
 # Константы размера окна
 WIDTH = 600
 HEIGHT = 300
+# Размеры корабля
+SPACESHIP_WIDTH = 55
+SPACESHIP_HEIGHT = 40
+# Скорость корабля
+VELOCITY = 1
 
 # Задаем размеры игрового окна
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -21,10 +27,6 @@ SPACE_BG = pygame.transform.scale(SPACE_IMAGE, (WIDTH, HEIGHT))
 
 # Формируем объект границы
 BORDER = pygame.Rect(WIDTH//2 - 2, 0, 4, HEIGHT)
-
-# Размеры корабля
-SPACESHIP_WIDTH = 55
-SPACESHIP_HEIGHT = 40
 
 # Загружаем изображение красного корабля 
 RED_SPACESHIP_IMAGE = pygame.image.load(
@@ -44,6 +46,37 @@ YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
 red = pygame.Rect(100, 150, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
 yellow = pygame.Rect(500, 150, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
 
+# Функция движения красного корабля
+def red_handle_movement(keys_pressed, red):
+    # Движение ВЛЕВО
+    if keys_pressed[pygame.K_a] and red.x - VELOCITY > 0:
+        red.x -= VELOCITY
+    # Движение ВПРАВО (запрещаем пересекать границу игроков)
+    if keys_pressed[pygame.K_d] and red.x + VELOCITY + red.width < BORDER.x:
+        red.x += VELOCITY
+    # Движение ВВЕРХ (запрещаем подниматься выше 0 по координате y)
+    if keys_pressed[pygame.K_w] and red.y - VELOCITY > 0:
+        red.y -= VELOCITY
+    # Движение ВНИЗ (запрещаем опускаться ниже чем высота экрана за вычетом высоты корабля и дополнительных 15 для отступа)
+    if keys_pressed[pygame.K_s] and red.y + VELOCITY + red.height < HEIGHT - 15:
+        red.y += VELOCITY
+
+# Функция движения желтого корабля
+def yellow_handle_movement(keys_pressed, yellow):
+    # Движение ВЛЕВО (запрещаем пересекать границу игроков)
+    if keys_pressed[pygame.K_LEFT] and yellow.x - VELOCITY > BORDER.x + BORDER.width:
+        yellow.x -= VELOCITY
+    # Движение ВПРАВО
+    if keys_pressed[pygame.K_RIGHT] and yellow.x + VELOCITY + yellow.width < WIDTH:
+        yellow.x += VELOCITY
+    # Движение ВВЕРХ
+    if keys_pressed[pygame.K_UP] and yellow.y - VELOCITY > 0:
+        yellow.y -= VELOCITY
+    # Движение ВНИЗ
+    if keys_pressed[pygame.K_DOWN] and yellow.y + VELOCITY + yellow.height < HEIGHT - 15:
+        yellow.y += VELOCITY
+
+
 # Запускаем бесконечный цикл программы
 # Это делается чтобы программа не завершалась и постоянно рисовала новые кадры игры
 while True:
@@ -61,6 +94,12 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+    # Узнаем нажатие клавишей
+    keys_pressed = pygame.key.get_pressed()
+    # Выполняем установку координат кораблей
+    red_handle_movement(keys_pressed, red)
+    yellow_handle_movement(keys_pressed, yellow)
 
     # Обновляем кадры игры
     pygame.display.update()
